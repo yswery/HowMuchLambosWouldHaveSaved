df29bcd66ca531bbeb930d9af7fa4c1c83100a11
Yif Swery <yiftachswr@gmail.com>
2017-11-13 23:41:03 +1300

    Add dummy ChartJS data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,8 +9,89 @@
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js"></script>
 
+
     </head>
     <body>
+        <div style="width:75%;">
+            <canvas id="canvas"></canvas>
+        </div>
+
+        <script>
+            window.chartColors = {
+                red: 'rgb(255, 99, 132)',
+                orange: 'rgb(255, 159, 64)',
+                yellow: 'rgb(255, 205, 86)',
+                green: 'rgb(75, 192, 192)',
+                blue: 'rgb(54, 162, 235)',
+                purple: 'rgb(153, 102, 255)',
+                grey: 'rgb(201, 203, 207)'
+            };
+
+            var ctx = document.getElementById("canvas").getContext('2d');
+
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: ["January", "February", "March", "April", "May", "June", "July"],
+                    datasets: [{
+                        label: "Value of BitCoin saved",
+                        backgroundColor: window.chartColors.red,
+                        borderColor: window.chartColors.red,
+                        data: [
+                            20,
+                            22,
+                            234,
+                            211
+                        ],
+                        fill: false,
+                    }, {
+                        label: "Accumulated money put in",
+                        fill: false,
+                        backgroundColor: window.chartColors.blue,
+                        borderColor: window.chartColors.blue,
+                        data: [
+                            20,
+                            40,
+                            60,
+                            80
+                        ],
+                    }]
+                },
+                options: {
+                    responsive: true,
+                    title:{
+                        display:true,
+                        text:'Historical Savings Gains'
+                    },
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false,
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Month'
+                            }
+                        }],
+                        yAxes: [{
+                            display: true,
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Value'
+                            }
+                        }]
+                    }
+                }
+            });
+
+
+        </script>
 
     </body>
 </html>
