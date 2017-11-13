a4f3ef265d0b6879d13dabfbf0672a69f5c88d2b
Yif Swery <yiftachswr@gmail.com>
2017-11-13 23:25:52 +1300

    Create the base blade view resource

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }}</title>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js"></script>
+
+    </head>
+    <body>
+
+    </body>
+</html>
